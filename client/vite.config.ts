82f61e8337c0a0807aa4d2b5269f46d943e0cbcb
athira <athiraomanakuttan20@gmail.com>
2025-07-22 11:31:28 +0530

    configuartion changes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -2,7 +2,11 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  preview: {
+    host: '0.0.0.0',
+    port: 4173, // default port, can be overridden by $PORT
+    allowedHosts: ['imagegallery-frontend.onrender.com']
+  }
 })
